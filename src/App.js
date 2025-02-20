f1e7772c6e284ab350b3e5abe3cbf724ba760dad
Rennie Mancho <122159220+Raeni-src@users.noreply.github.com>
2025-02-20 11:15:58 +0100

    uncommented

--- src/App.js
+++ src/App.js
@@ -6,17 +6,17 @@ import './App.css';
 import LaunchAnimation from './components/LaunchAnimation/LaunchAnimation'; // Import Launch Animation
 //import  NavigationLoader from './components/Loader/NavigationLoader';
 // Lazy-loaded components
-//const LoginPage = lazy(() => import('./pages/Login.jsx'));
-//const SignupPage = lazy(() => import('./pages/Signup.jsx'));
-//const ForgotPasswordPage = lazy(() => import('./pages/ForgotPassword.jsx'));
-//const Home = lazy(() => import('./pages/Home.jsx'));
+const LoginPage = lazy(() => import('./pages/Login.jsx'));
+const SignupPage = lazy(() => import('./pages/Signup.jsx'));
+const ForgotPasswordPage = lazy(() => import('./pages/ForgotPassword.jsx'));
+const Home = lazy(() => import('./pages/Home.jsx'));
 
-//const Shops = lazy(() => import('./pages/Shops.jsx'));
-//const ProductPage = lazy(() => import('./pages/ProductPage.jsx'));
-//const CartPage = lazy(() => import('./pages/CartPage.jsx'));
-//const ProfilePage = lazy(() => import('./pages/ProfilePage.jsx'));
-//const ShopView = lazy(() => import('./components/shops/ShopView.jsx'));
-//const ShopProfile = lazy(() => import('./components/shops/ShopProfile.jsx'));
+const Shops = lazy(() => import('./pages/Shops.jsx'));
+const ProductPage = lazy(() => import('./pages/ProductPage.jsx'));
+const CartPage = lazy(() => import('./pages/CartPage.jsx'));
+const ProfilePage = lazy(() => import('./pages/ProfilePage.jsx'));
+const ShopView = lazy(() => import('./components/shops/ShopView.jsx'));
+const ShopProfile = lazy(() => import('./components/shops/ShopProfile.jsx'));
 
 
 const App = () => {
@@ -36,25 +36,24 @@ const App = () => {
  
         {/*Main Content after launch Animation */}
         
-   {/* <Suspense fallback={<NavigationLoader />}>
+   <Suspense fallback={<NavigationLoader />}>
     <Routes>
 
-         <Route path="/login" element={<LoginPage />} /> {/* Explicit login route
-        <Route path="/signup" element={<SignupPage />} /> {/* Signup route 
+         <Route path="/login" element={<LoginPage />} /> 
+        <Route path="/signup" element={<SignupPage />} />  
         <Route path="/forgot-password" element={<ForgotPasswordPage />} /> 
 
         <Route path="/home" element={<Home />} />
         <Route path="/shops" element={<Shops />} />
         <Route path="/product/:productId" element={<ProductPage />} />
-        */}
-        {/* 
+         
         <Route path="/cart" element={<CartPage />} />
         <Route path="/profile" element={<ProfilePage />} />
         <Route path="/shop-view/:shopId" element={<ShopView />} />
         <Route path="/shop-profile/:shopId" element={<ShopProfile />} />
       </Routes>
 
-      </Suspense>*/}
+      </Suspense>
     </Router>
   );
 };
