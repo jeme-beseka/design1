522eeb17ed1c6f8df8bd10018d9754a692e348a5
Jeme Beseka <j.beseka@gmail.com>
2025-02-26 10:09:14 +0100

    Fixed App.js conflict

--- src/App.js
+++ src/App.js
@@ -5,17 +5,14 @@ import DashboardContent from './vendor/DashboardContent.jsx';
 import Dashboard from './vendor/Dashboard';
 import './App.css';
 
-
 import { ProductModalProvider } from './components/context/ProductModalContext'; 
 import LaunchAnimation from './components/LaunchAnimation/LaunchAnimation'; // Import Launch Animation
-import  NavigationLoader from './components/Loader/NavigationLoader';
-
+import NavigationLoader from './components/Loader/NavigationLoader';
 
 const LoginPage = lazy(() => import('./pages/Login.jsx'));
 const SignupPage = lazy(() => import('./pages/Signup.jsx'));
 const ForgotPasswordPage = lazy(() => import('./pages/ForgotPassword.jsx'));
 const Home = lazy(() => import('./pages/Home.jsx'));
-
 const Shops = lazy(() => import('./pages/Shops.jsx'));
 const ProductPage = lazy(() => import('./pages/ProductPage.jsx'));
 const CartPage = lazy(() => import('./pages/CartPage.jsx'));
@@ -24,57 +21,43 @@ const ShopView = lazy(() => import('./components/shops/ShopView.jsx'));
 const ShopProfile = lazy(() => import('./components/shops/ShopProfile.jsx'));
 const ProductDetailModal = lazy(() => import('./components/products/ProductDetailModal.jsx'));
 
-
 const App = () => {
     const [isLoading, setIsLoading] = useState(true);
 
-  // //   // Function to handle animation completion
-  const handleAnimationComplete = () => {
-    setIsLoading(false);
-  };
-
+    // Function to handle animation completion
+    const handleAnimationComplete = () => {
+        setIsLoading(false);
+    };
 
     return (
-      <Router>
-        {isLoading && (
-          <LaunchAnimation onAnimationComplete={handleAnimationComplete} />
-        )}
- 
-  {/*Main Content after launch Animation */}
-        
- <Suspense fallback={<NavigationLoader />}> 
-   
-    <ProductModalProvider>
-      <div className="app-container">
-          <Routes>
-
-         <Route path="/login" element={<LoginPage />} /> 
-        <Route path="/signup" element={<SignupPage />} />  
-        <Route path="/forgot-password" element={<ForgotPasswordPage />} /> 
-
-        <Route path="/home" element={<Home />} />
-        <Route path="/shops" element={<Shops />} />
-        <Route path="/product/:productId" element={<ProductPage />} />
-         
-        <Route path="/cart" element={<CartPage />} />
-        <Route path="/profile" element={<ProfilePage />} />
-        <Route path="/shop-view/:shopId" element={<ShopView />} />
-        <Route path="/shop-profile/:shopId" element={<ShopProfile />} />
-        <Route path="/vendor" element={<VendorDashboard />} /> 
-        <Route path="/vendor-dashboard" element={<DashboardContent />} /> 
-      </Routes>
-        <ProductDetailModal />
-      </div>
-    </ProductModalProvider>
-
-      </Suspense>
-    </Router>
-  );
+        <Router>
+            {isLoading && (
+                <LaunchAnimation onAnimationComplete={handleAnimationComplete} />
+            )}
+            {/* Main Content after launch Animation */}
+            <Suspense fallback={<NavigationLoader />}>
+                <ProductModalProvider>
+                    <div className="app-container">
+                        <Routes>
+                            <Route path="/login" element={<LoginPage />} /> 
+                            <Route path="/signup" element={<SignupPage />} />  
+                            <Route path="/forgot-password" element={<ForgotPasswordPage />} /> 
+                            <Route path="/home" element={<Home />} />
+                            <Route path="/shops" element={<Shops />} />
+                            <Route path="/product/:productId" element={<ProductPage />} />
+                            <Route path="/cart" element={<CartPage />} />
+                            <Route path="/profile" element={<ProfilePage />} />
+                            <Route path="/shop-view/:shopId" element={<ShopView />} />
+                            <Route path="/shop-profile/:shopId" element={<ShopProfile />} />
+                            <Route path="/vendor" element={<VendorDashboard />} /> 
+                            <Route path="/vendor-dashboard" element={<DashboardContent />} /> 
+                        </Routes>
+                        <ProductDetailModal />
+                    </div>
+                </ProductModalProvider>
+            </Suspense>
+        </Router>
+    );
 };
 
 export default App;
-
-
-
-
-
